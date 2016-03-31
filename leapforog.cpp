3571e556438a4d8eec88472a8cd90d93c3578bc4
Lee Eun-Chan <hd132620@users.noreply.github.com>
2016-03-31 18:37:03 +0900

    Added files via upload

---
+++ leapforog.cpp
@@ -0,0 +1,36 @@
+//
+// Created by USER on 2016-03-31.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    bool updatePos = true;
+
+    double s = 500.0;
+    double v12 = 0.0;
+    double t = 0.0, dT = 0.01;
+
+    v12 += -9.8 * (dT/2);
+
+    while(s > 0)
+    {
+        if(updatePos)
+        {
+            cout<<"시간 : "<< t <<"초, 위치 : "<< s <<"m, 속도+(dT/2) : "<< v12 <<"m/s"<<endl;
+            s += v12 * dT;
+            updatePos = false;
+            t += dT;
+        }
+        else
+        {
+            v12 += -9.8 * dT;
+            updatePos = true;
+        }
+
+    }
+
+    return 0;
+}
